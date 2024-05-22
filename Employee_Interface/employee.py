293705fda5937981dbd70eafded6ed3d2e2f5d5c
Gerasimos Gryllias <157423043+grylliasg@users.noreply.github.com>
2024-05-22 20:41:22 +0300

    Add files via upload
    classes.py = classes task, meeting, leave
    connect.py = connection to database
    employee.py = class user, employee + methods

---
+++ Employee_Interface/employee.py
@@ -0,0 +1,256 @@
+from tkinter import *
+from tkinter import messagebox
+from tkcalendar import DateEntry
+from datetime import datetime
+import connect
+from classes import *
+
+class User:
+    def __init__(self, name, id, username, password, role):
+        self.name = name
+        self.id = id
+        self.username = username
+        self.password = password
+        self.role = role
+
+    def profileSettings(self):
+        profSettIf = Tk()
+        profSettIf.geometry('350x200')
+        profSettIf.title('Profile Settings')
+
+        def leave():
+            profSettIf.destroy()
+
+        def setName():
+            name.forget()
+            pwrd.forget()
+            leave.forget()
+
+            def submitName():
+                if self.name == nameEntry.get():
+                    messagebox.showwarning('Wrong Name', 'You entered your existing name.')
+                    nameEntry.delete(0, END)
+                    nameEntry.delete(0, END)
+                else:
+                    self.name = nameEntry.get()
+                    query = "UPDATE users SET name = %s WHERE UserID = %s"
+                    connect.cursor.execute(query, (self.name, self.id))
+                    connect.conn.commit()
+                    messagebox.showinfo('Success', f'New Name = {self.name}')
+                    profSettIf.destroy()
+
+            nameLabel = Label(profSettIf, text='Enter Your New name:')
+            nameLabel.pack()
+            nameEntry =  Entry(profSettIf)
+            nameEntry.pack()
+            submit = Button(profSettIf, text='Submit', cursor='hand2',  command=submitName)
+            submit.pack()
+
+
+        def setPassword():
+            name.forget()
+            pwrd.forget()
+            leave.forget()
+
+            def submitPassword():
+                if self.password != oldEntry.get():
+                    messagebox.showerror('Wrong Password', 'The Old password you entered is wrong')
+                    oldEntry.delete(0, END)
+                    newEntry.delete(0, END)
+                else:
+                    self.password = newEntry.get()
+                    query = "UPDATE users SET password = %s WHERE name = %s"
+                    connect.cursor.execute(query, (self.password, self.name))
+                    connect.conn.commit()
+                    messagebox.showinfo('Success', 'Your Password has been changed')
+                    profSettIf.destroy()
+
+
+            oldLabel = Label(profSettIf, text='Enter Your Old password:')
+            oldLabel.pack()
+            oldEntry =  Entry(profSettIf, show='*')
+            oldEntry.pack()
+            newLabel = Label(profSettIf, text='Enter Your New password:')
+            newLabel.pack()
+            newEntry =  Entry(profSettIf, show='*')
+            newEntry.pack()
+            submit = Button(profSettIf, text='Submit', cursor='hand2',  command=submitPassword)
+            submit.pack()
+
+        name = Button(profSettIf, text='Change Your Name', cursor='hand2', command= setName)
+        name.pack()
+        pwrd = Button(profSettIf, text='Change Your Password', cursor='hand2', command= setPassword)
+        pwrd.pack()
+        leave = Button(profSettIf, text='Aποχώρηση', cursor='hand2', command= leave)
+        leave.pack()
+
+        profSettIf.mainloop()
+
+
+class Employee(User):
+    def __init__(self, name, id, username, password, role, assignedTasks, meetingSchedule, leaveRequests):
+        super().__init__(name, id, username, password, role)
+        self.assignedTasks = assignedTasks
+        self.meetingSchedule = meetingSchedule
+        self.leaveRequests = leaveRequests
+
+    def getAssignedTasks(self):
+        return self.assignedTasks
+    
+    def getMeetings(self):
+        return self.meetingSchedule
+    
+    def check_team_invitation(self):
+        pass
+    
+    def show_completed_tasks(self):
+        global win
+        win = Tk()
+        win.geometry('350x450')
+
+        task_label = Label(win, text='Completed Tasks:', font=('Arial', 15))
+        task_label.place(x=5, y=0)
+        main_comtaskframe = Frame(win, bg="lightgrey", bd=4)
+        main_comtaskframe.place(x=90, y=90)
+
+        for assignedTask in self.assignedTasks:
+            if assignedTask.completed == 'Completed':
+                comtasklabel = Button(main_comtaskframe, text=assignedTask.title, height=5, width=15, cursor='hand2',  command= lambda assignedTask=assignedTask: self.view_assigned_task(assignedTask)).pack()
+            else:
+                pass
+    
+    def view_assigned_task(self, task):
+        taskwin = Tk()
+        taskwin.geometry('350x350')
+
+        def mark_task_complete():
+            query = 'UPDATE tasks SET TaskStatus = %s WHERE TaskName = %s'
+            connect.cursor.execute(query, ('Completed', task.title))
+            connect.conn.commit()
+            task.completed = 'Completed'
+            taskwin.destroy()
+
+        def unmark_completed_task():
+            query = 'UPDATE tasks SET TaskStatus = %s WHERE TaskName = %s'
+            connect.cursor.execute(query, ('Uncompleted', task.title))
+            connect.conn.commit()
+            task.completed = 'Uncompleted'
+            taskwin.destroy()
+            win.destroy()
+            
+
+        task_details = Label(taskwin, text=f'Task Title: {task.title}\n\nDescription: {task.description}\n\nDeadline: {task.deadline}\n', font=('Arial', 15))
+        task_details.pack()
+
+        if task.completed == 'Uncompleted':
+            complete = Button(taskwin, text='Complete this Task', bg='green', fg='white', font=('Arial', 15), border=2, cursor='hand2', command= mark_task_complete)
+            complete.pack()
+        else:
+            completed = Label(taskwin, text='This Task is Completed!', font=('Arial', 15), fg='green').pack()
+            unmark = Button(taskwin, text='Unmark Completed Task', font=('Arial, 13'), cursor='hand2', fg='orange', command= unmark_completed_task).pack()
+
+    def view_meeting_schedule(self, meeting):
+        meetwin = Tk()
+        meetwin.geometry('350x350')
+        meetwin.title('Meeting Details')
+
+        def attend_meeting(meeting):
+            self.meetingSchedule.append(meeting)
+            meetwin.destroy()
+
+        def request_not_attend_meeting(meeting):
+            self.meetingSchedule.remove(meeting)
+            meetwin.destroy()
+            
+
+        meeting_details = Label(meetwin, text=f'Meeting Title: {meeting.meetingName}\n\nDescription: {meeting.date}\n\n', font=('Arial', 15))
+        meeting_details.pack()
+
+        if meeting in self.meetingSchedule:
+            notattend = Button(meetwin, text='Not Attend', fg='red', font=('Arial', 12), cursor='hand2', command=lambda:request_not_attend_meeting(meeting)).pack()
+        else:
+            notattendlabel = Label(meetwin, text="You're not attending this meeting.", font=('Arial', 14), fg='red').pack()
+            unmark = Button(meetwin, text='Click to Attend', font=('Arial, 13'), cursor='hand2', fg='orange', command= lambda:attend_meeting(meeting)).pack()
+
+    def request_leave(self):
+        leavewin = Tk()
+        leavewin.geometry('350x350')
+        leavewin.title('Leave Request')
+
+        def submit_leave_request(start, end, name):
+
+            # ELEGXOS AN O EMPLOYEE EXEI HDH ENA APODEKTO LEAVE POU TON PERIMENEI
+            hasrequest = 'SELECT LeaveRequestID FROM leaverequests WHERE UserID = %s AND LeaveStatus = %s'
+            connect.cursor.execute(hasrequest, (self.id, 'Αποδεκτή'))
+            hasrequestres = connect.cursor.fetchall()
+
+            if hasrequestres: # APOTELESMA ELEGXOU = TRUE
+                messagebox.showwarning('Failure', "You've got already an approved request")
+                leavewin.destroy()
+
+            else: # ALLIWS, ELEGXOS AN OI HMEROMINIES POU EVALE PEFTOUN MESA SE ALLO APODEKTO LEAVE
+                apprdates = 'SELECT LeaveStartDate, LeaveEndDate FROM leaverequests WHERE LeaveStatus = %s'
+                connect.cursor.execute(apprdates, ('Αποδεκτή', ))
+                apprdates_res = connect.cursor.fetchall()
+
+                for date in apprdates_res: # GIA KATHE APODEKTO LEAVE REQUEST TSEKARE TIS HMEROMHNIES
+                    if date[0] <= start <= date[1] or date[0] <= end <= date[1]:
+                        messagebox.showwarning('Failure', "There is another approved request between these dates")
+                        leavewin.destroy()
+                    else:
+                        submitleave = 'INSERT INTO leaverequests VALUES (null, %s, %s, %s, %s, %s)'
+                        connect.cursor.execute(submitleave, (start, end, 'Υπο εξέταση', self.id, name))
+                        connect.conn.commit()
+                        messagebox.showinfo('Submitted', f"Your leave request ({start} - {end}) has been submitted!")
+                        leavewin.destroy()
+            
+
+        label = Label(leavewin, text="Submit Your Leave Request", font=('Arial', 15)).pack()
+
+        namelabel = Label(leavewin, text='Request Name:', font=('Arial', 12)).pack(pady=(10,0))
+        name = Entry(leavewin)
+        name.pack(pady=(0,15))
+
+        sdatelabel = Label(leavewin, text='Start Date:', font=('Arial', 12))
+        sdatelabel.pack(pady=(15,0))
+        start = DateEntry(leavewin, width=17, background="darkblue", foreground="white", borderwidth=2)
+        start.pack(pady=(0,15))
+
+        edatelabel = Label(leavewin, text='End Date:', font=('Arial', 12))
+        edatelabel.pack()
+        end = DateEntry(leavewin, width=17, background="darkblue", foreground="white", borderwidth=2)
+        end.pack(pady=(0,50))
+
+        submit = Button(leavewin, text='Submit', font=('Arial', 12), bg='lightblue', cursor='hand2', border=3, command=lambda: submit_leave_request(start.get_date(), end.get_date(), name.get()))
+        submit.pack()
+
+        leavewin.mainloop()
+
+    def show_request_status(self):
+        statuswin = Tk()
+        statuswin.geometry('450x450')
+        statuswin.title('Leave Request Status')
+
+        status = 'SELECT LeaveStatus, LeaveRequestID, LeaveStartDate, LeaveEndDate, LeaveName FROM leaverequests WHERE UserID = %s'
+        connect.cursor.execute(status, (self.id, ))
+        res = connect.cursor.fetchall()
+
+        scrollbar = Scrollbar(statuswin, orient=VERTICAL)
+        scrollbar.pack(side="right", fill="y")
+
+        text = Text(statuswin, wrap="word", yscrollcommand=scrollbar.set)
+        text.pack(expand=True, fill="both")
+        scrollbar.config(command=text.yview)
+
+        for r in res:
+            status_res = r[0]
+            id = r[1]
+            start = r[2]
+            end = r[3]
+            name = r[4]
+
+            text.insert(END, f'The Status of your request is: {status_res}\n\n')
+            text.insert(END, f'Leave Request Id: {id}\nLeave Start: {start}\nLeave End: {end}\nLeave Name: {name}\n\n')
+
+        statuswin.mainloop()
+
---
+++ Employee_Interface/login_register_v02.py
@@ -0,0 +1,435 @@
+# CONNECT TO PROGRESSPULSE DATABASE AND LOGIN TO AN ACCOUNT
+from tkinter import *
+from tkinter import messagebox
+from employee import *
+import connect
+
+# Function for Login (+ Check credentials)
+def login(username, password, window):
+    #EMPLOYEE __init__(self, name, id, username, password, role, assignedTasks, meetingSchedule, leaveRequests):
+    query = "SELECT name, UserID, Username, Password, UserRole FROM users WHERE Username = %s AND Password = %s"
+    connect.cursor.execute(query, (username, password))
+    result = connect.cursor.fetchall()
+      
+    if result: # FTIAKSE TON EMPLOYEE XWRIS TASKS, MEETINGS, LEAVES
+        userID = result[0][1]
+        globals()[username] = Employee(result[0][0], result[0][1], result[0][2], result[0][3], result[0][4], [], [], [])
+
+        # PSAKSE GIA tasks TOY EMPLOYEE 
+        query2 = "SELECT TaskName, TaskDescription, TaskDeadline, TaskStatus FROM tasks WHERE UserID = %s"
+        connect.cursor.execute(query2, (userID, ))
+        result2 = connect.cursor.fetchall()
+
+        if result2: # AMA YPARXOYN TASKS, FTIAKSE TA OBJECTS KAI VALTA STON EMPLOYEE.ASSIGNEDTASKS
+            for task in result2:
+                taskname = task[0]
+                description = task[1]
+                deadline = task[2]
+                iscompleted = task[3]
+                globals()[taskname] = Task(taskname, description, deadline, iscompleted) # DHMIOURGIA TOY OBJECT TASK
+                globals()[username].assignedTasks.append(globals()[taskname])
+
+        # VRES SE POIA OMADA ANHKEI AUTOS O EMPLOYEE
+        quer = "SELECT Team FROM users WHERE UserID = %s"
+        connect.cursor.execute(quer, (userID, ))
+        resul = connect.cursor.fetchall()
+        histeam = resul[0][0]
+
+        # FERE TA MEETINGS POY ANHKOUN STHN OMADA TOY 
+        query3 = "SELECT MeetingName, MeetingDateTime, MeetingAgenda FROM meetings WHERE TeamID = %s"
+        connect.cursor.execute(query3, (histeam, ))
+        result3 = connect.cursor.fetchall()
+
+        if result3: # AN YPARXOUN MEETINGS
+            for meeting in result3:
+                meetingname = meeting[0]
+                meetingdate = meeting[1]
+                globals()[meetingname] = Meeting(meetingname, meetingdate) # DHMIOURGIA TOY OBJECT MEETING
+                globals()[username].meetingSchedule.append(globals()[meetingname]) # PROSTHIKI STO meeting.schedule
+
+        # FERE TA LEAVES
+        query4 = "SELECT LeaveName, LeaveStartDate, LeaveEndDate FROM leaverequests WHERE UserID = %s"
+        connect.cursor.execute(query4, (userID, ))
+        result4 = connect.cursor.fetchall()
+
+        if result4:
+            for leave in result4:
+                leavename = leave[0]
+                start = leave[1]
+                end = leave[2]
+                globals()[leavename] = Leave(leavename, start, end) # DHMIOURGIA TOY OBJECT LEAVE
+                globals()[username].leaveRequests.append(globals()[leavename])
+
+        if histeam != None: # AN ANHKEI SE MIA OMADA => REDIRECT TO employeeInterface
+            window.destroy()
+            employeeInterface(globals()[username], histeam)
+
+        else:
+            window.destroy()
+            
+            global teamNone
+            teamNone = Tk()
+            teamNone.geometry('600x550')
+            teamNone.title('User Interface')
+
+            label = Label(teamNone, text='You are not working for a team.', font=('Arial', 15)).pack(pady=(0,20))
+            checkinv = Button(teamNone, text='Check for Invitation', cursor='hand2', command= globals()[username].check_team_invitation).pack()
+
+            logout = Button(teamNone,
+                text='Logout',
+                font=('Arial', 12, 'bold'),
+                fg='white',
+                bg='tomato',
+                cursor='hand2',
+                padx=10,
+                pady=5,
+                command=lambda: (teamNone.destroy(), createWindow()))
+
+            logout.place(x=520, y=25)
+
+
+    else:
+        messagebox.showerror("Invalid Credentials", "Invalid Username or Password, please try again")
+        window.destroy()
+        createWindow()
+
+  
+def employeeInterface(name, team):
+    global if1
+
+    if1 = Tk()
+    if1.attributes('-fullscreen', True)
+    if1.title('User Interface')
+
+    limg= Label(if1, bg='lightblue')
+    limg.place(relheight=1, relwidth=1)
+
+    logout = Button(if1,
+                    text='Logout',
+                    font=('Arial', 10),
+                    fg='white',
+                    bg='red',
+                    cursor='hand2',
+                    command= lambda: (if1.destroy(), createWindow())
+                    )
+    logout.place(x=1470, y=15)
+
+    welcome = Label(if1, 
+                text=f'Welcome, {name.name}', 
+                font=('Helvetica', 18, 'bold italic'), 
+                border=5, 
+                relief="groove", 
+                bg='lightblue', 
+                padx=10, 
+                pady=5)
+    welcome.place(x=20, y=20)
+
+    # VRES TO ONOMA TOU TEAM POU ANHKEI
+    teamName = "SELECT TeamName FROM teams WHERE TeamID = %s"
+    connect.cursor.execute(teamName, (team, ))
+    resul = connect.cursor.fetchall()
+    teamName_res = resul[0][0]
+
+    teamlabel = Label(if1, 
+                 text=f'Current Team: {teamName_res}', 
+                 font=('Arial', 12), 
+                 fg='grey', 
+                 bg='lightblue',
+                 border=3,
+                 padx=5, 
+                 pady=3)
+    teamlabel.place(x=20, y=80)
+
+    settings = Button(if1,
+                    text='Ρυθμίσεις Λογαριασμού',
+                    font=('Arial', 10),
+                    fg='black',
+                    bg='white',
+                    cursor='hand2',
+                    command= name.profileSettings
+                    )
+    settings.place(x=1300, y=15)
+
+    # TASKS
+    task_label = Label(if1, 
+                   text='my Tasks', 
+                   font=('Helvetica', 15, 'bold italic'), 
+                   bg='lightsteelblue', 
+                   padx=10, 
+                   pady=5)
+
+    task_label.place(x=348, y=90)
+    main_taskframe = Frame(if1, bg="lightgrey", bd=4)
+    main_taskframe.place(x=345, y=130)
+
+    assignedTasks = name.getAssignedTasks()
+    if all(assignedTask.completed == 'Completed' for assignedTask in assignedTasks) or len(assignedTasks) == 0:
+        notasks = Label(main_taskframe, text='You have not active tasks')
+        notasks.pack()
+    for assignedTask in assignedTasks:
+        if assignedTask.completed == 'Uncompleted':
+            task = Button(main_taskframe, text=assignedTask.title, height=5, width=15, cursor='hand2',  command= lambda assignedTask=assignedTask: name.view_assigned_task(assignedTask))  # Adjust height and width as needed
+            task.pack()
+
+
+    # MEETINGS
+    meet_label = Label(if1, 
+                   text='my Meetings', 
+                   font=('Helvetica', 15, 'bold italic'), 
+                   bg='lightsteelblue', 
+                   padx=10, 
+                   pady=5)
+
+    meet_label.place(x=618, y=90)
+    meetframe = Frame(if1, bg="lightgrey", bd=4)
+    meetframe.place(x=630, y=130)
+
+    meetings = name.getMeetings()
+    if len(meetings) == 0:
+        nomeets = Label(meetframe, text='You have not active meetings')
+        nomeets.pack()
+    for meeting in meetings:
+        task = Button(meetframe, text=meeting.meetingName, height=5, width=15, cursor='hand2', command= lambda meeting=meeting:name.view_meeting_schedule(meeting))  # Adjust height and width as needed
+        task.pack()
+
+    # CHAT
+    chatLabel = Label(if1, 
+                   text='Chat:', 
+                   font=('Helvetica', 15, 'bold italic'), 
+                   bg='lightsteelblue', 
+                   padx=10, 
+                   pady=5)
+    chatLabel.place(x=990, y=90)
+    chat = Text(if1, height=20, width=30)
+    chat.place(x=990, y=130)
+
+    # Completed Tasks
+    # Create a button to show completed tasks
+    comTasks = Button(if1, 
+                  text='Show Completed Tasks', 
+                  fg='white', 
+                  bg='green', 
+                  cursor='hand2', 
+                  font=('Arial', 12), 
+                  command=name.show_completed_tasks)
+
+    comTasks.place(x=5, y=690)
+
+    # Make Leave Request
+    newLeave = Button(if1, text='Make Leave Request', fg='white', bg='blue', cursor='hand2', font=('Arial', 12), command= name.request_leave)
+    newLeave.place(x=5, y=730)
+
+    # SEE THE LEAVE REQUEST STATUS
+    leavereqButton = Button(if1, text='See Leave Status', cursor='hand2', font=('Arial', 10), bg='grey', fg='white', command= name.show_request_status)
+    leavereqButton.place(x=5, y=770)
+
+    if1.mainloop()
+
+
+# Function to create a new account 
+def register(fname, lname, username, password, password2, window):
+    if password != password2:
+        messagebox.showerror("Error", "Passwords don't match, try again")
+        forgetRegisterWidgets()
+        registerWidgets(Event)
+    elif len(password) == 0:
+        messagebox.showerror("Error", "Password must be at least one (1) character, try again")
+        forgetRegisterWidgets()
+        registerWidgets(Event)
+    else:
+        # SQL QUERY
+        query = "INSERT INTO users VALUES (null, %s, %s, %s, %s, null)"
+        connect.cursor.execute(query, (username, password, 'employee', f'{fname} {lname}', ))
+        connect.conn.commit()
+        messagebox.showinfo("Success", f"Your account has been created, {fname}!")
+        window.destroy()
+        createWindow()
+
+def forgetRegisterWidgets():
+    widgets_to_forget = [
+                        fname_label,
+                        fname_entry,
+                        lname_label,
+                        lname_entry,
+                        reg_username_label,
+                        reg_username_entry,
+                        reg_password_label,
+                        reg_password_entry,
+                        reg_password2_label,
+                        reg_password2_entry,
+                        register_button,
+                        ]
+    
+    for widget in widgets_to_forget:
+        widget.forget()
+
+
+# Function to make transition from login to register
+def forgetLoginWidgets():
+    # Forget the Login Widgets
+    widgets_to_forget = [username_label, 
+                         username_entry, 
+                         password_label,
+                         password_entry, 
+                         login_button, 
+                         clickForRegisterLabel, 
+                         clickForRegister,
+                         ]
+    
+    for widget in widgets_to_forget:
+        widget.forget()
+
+def registerWidgets(event):
+    global fname_label
+    global fname_entry
+    global lname_label
+    global lname_entry
+    global reg_username_label
+    global reg_username_entry
+    global reg_password_label
+    global reg_password_entry
+    global reg_password2_label
+    global reg_password2_entry
+    global register_button
+
+    if username_label:
+        forgetLoginWidgets()
+    # Create new widgets for register
+    window.geometry("350x420")
+    window.title("ProgressPulse Register")
+
+    fname_label = Label(window,
+                        text="Enter your first name:",
+                        padx=10,
+                        pady=10,
+                        )
+    fname_label.pack()
+    fname_entry = Entry(window)
+    fname_entry.pack()
+
+    lname_label = Label(window,
+                        text="Enter your last name:",
+                        padx=10,
+                        pady=10,
+                        )
+    lname_label.pack()
+    lname_entry = Entry(window)
+    lname_entry.pack()
+
+    reg_username_label = Label(window,
+                        text="Enter your username:",
+                        padx=10,
+                        pady=10,
+                        )
+    reg_username_label.pack()
+    reg_username_entry = Entry(window)
+    reg_username_entry.pack()
+
+    reg_password_label = Label(window,
+                        text="Enter your password:",
+                        padx=10,
+                        pady=10,
+                        )
+    reg_password_label.pack()
+    reg_password_entry = Entry(window, show="*")
+    reg_password_entry.pack()
+
+    reg_password2_label = Label(window,
+                        text="reEnter your password:",
+                        padx=10,
+                        pady=10,
+                        )
+    reg_password2_label.pack()
+    reg_password2_entry = Entry(window, show="*")
+    reg_password2_entry.pack()
+
+    register_button = Button(window,
+                         text="Sign Up",
+                         border=3,
+                         padx=15,
+                         pady=5,
+                         cursor='hand2',
+                         command=lambda: register(fname_entry.get(), lname_entry.get(), reg_username_entry.get(), reg_password_entry.get(), reg_password2_entry.get(), window)
+                         )
+    register_button.pack()
+
+
+
+
+def createWindow():
+    global window
+    global welcome_label
+    global username_label
+    global username_entry
+    global password_label
+    global password_entry
+    global login_button
+    global clickForRegisterLabel
+    global clickForRegister
+
+    window = Tk()
+    window.geometry("350x300")
+    window.title("ProgressPulse Login")
+
+    # Welcome label
+
+    welcome_label = Label(window,
+                        text="  Welcome to \n ProgressPulse",
+                        font=("Arial", 20, "bold"),
+                        fg="#2456ed"
+                        )
+    welcome_label.pack()
+
+    # Username Label and Entry
+    username_label = Label(window,
+                            text="Username:",
+                            padx=10,
+                            pady=10,
+                            )
+    username_label.pack()
+    username_entry = Entry(window)
+    username_entry.pack()
+
+    # Password Label and Entry
+    password_label = Label(window,
+                            text="Password:")
+    password_label.pack()
+    password_entry = Entry(window, show="*")  # Show * for password
+    password_entry.pack()
+
+    # Login button
+    login_button = Button(window,
+                        text="Login",
+                        border=3,
+                        padx=15,
+                        pady=5,
+                        cursor='hand2',
+                        command=lambda: login(username_entry.get(), password_entry.get(), window)
+                        )
+    login_button.pack()
+
+    # Regiter option label
+    clickForRegisterLabel = Label(window,
+                    text="If you don't have an account \n create one",
+                    pady=5)
+    clickForRegisterLabel.pack()
+
+    # "here" button to go for create account
+    clickForRegister = Label(window,
+                text="here",
+                fg="blue",
+                cursor="hand2",
+                )
+    clickForRegister.pack()
+    clickForRegister.bind("<Button-1>", registerWidgets)
+
+
+    window.mainloop()
+
+# RUN window function to start the program ...
+if __name__ == "__main__":
+    connect.connectToDatabase()
+    createWindow()
+
+
+
